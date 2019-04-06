0e038f5ab6b2d9d14ddc13be06d13008d9d2c4d8
Laugel Yannick <laugel-yannick@hotmail.fr>
2019-04-06 16:06:23 +0200

    Ajout de la vue de l'edition de post + requête UPDATE

--- admin/dashboard/supprimer.php
+++
@@ -1,9 +0,0 @@
-<?php
-$bdd = new PDO();
-if(isset($_GET['id']) AND !empty($_GET['id'])) {
-   $suppr_id = htmlspecialchars($_GET['id']);
-   $suppr = $bdd->prepare('DELETE FROM T_BILLET WHERE BIL_ID = ?');
-   $suppr->execute(array($suppr_id));
-   header('Location: http://projet4.alwaysdata.net/admin/dashboard/index.php');
-}
-?>
