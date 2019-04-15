c316ef7a697abd3e83a576e35f3cd5e8fba46a56
Laugel Yannick <laugel-yannick@hotmail.fr>
2019-04-15 13:47:53 +0200

    Suppression des anciesn fichiers

--- admin/dashboard/viewError.php
+++
@@ -1,9 +0,0 @@
-<?php $title = 'Erreur'; ?>
-
-<?php ob_start() ?>
-
-    <p>Une erreur est survenue : <?= $msgErreur ?></p>
-
-<?php $content = ob_get_clean(); ?>
-
-<?php require 'template.php'; ?>
