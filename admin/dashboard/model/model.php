0b201d6e18df4043c49ef6336517724de27b615f
Laugel Yannick <laugel-yannick@hotmail.fr>
2019-04-15 14:44:03 +0200

    Passage en POO, class Model devient abstraite

--- admin/dashboard/model/model.php
+++ admin/dashboard/model/model.php
@@ -1,67 +1,32 @@
 <?php
-    
-function getBdd(){
 
-    $bdd = new PDO(',
-                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-    return $bdd;
-}  
+abstract class Modele {
 
+  // Objet PDO d'accès à la BD
+  private $bdd;
 
-function getBillets(){
-    
-    $bdd = getBdd();
-    $billets = $bdd->query('SELECT BIL_ID as id, BIL_DATE as date, BIL_TITRE as titre, 
-    BIL_CONTENU as contenu FROM T_BILLET ORDER BY BIL_ID DESC');
-    return $billets;
-}
-
-function getPost($idPost){
-
-    $bdd = getBdd();
-    $post = $bdd->prepare('SELECT BIL_ID as id, BIL_DATE as date, BIL_TITRE as title, 
-    BIL_CONTENU as content FROM T_BILLET WHERE BIL_ID = ?');
-    $post->execute(array($idPost));
-    if($post->rowCount() == 1)
-        return $post->fetch();
-    else
-        throw new Exception("Aucun billet ne correspond à l'identifiant '$idPost'");
-}
-
-function addPost(){
-
-    $bdd = getBdd();
-    $req = $bdd->prepare('INSERT INTO T_BILLET (BIL_TITRE, BIL_CONTENU, BIL_DATE)
-                          VALUES (?, ?, NOW())');
-    $req->execute(array($post_title, $post_content));
-    return $req;
-}
-
-function deletePost(){
-
-    $bdd = getBdd();
-    $delete = $bdd->prepare('DELETE FROM T_BILLET WHERE BIL_ID = ?');
-    $delete->execute(array($delete_id));
-    return $delete;
-}
-
-function editPost(){
-
-    $bdd = getBdd($edit_id);
-    $edit_post = $bdd->prepare('SELECT * FROM T_BILLET WHERE BIL_ID = ?');
-    $edit_post->execute(array($edit_id));
-    if($edit_post->rowCount() == 1){
-        return $edit_post->fetch();
+  // Exécute une requête SQL éventuellement paramétrée
+  protected function executerRequete($sql, $params = null) {
+    if ($params == null) {
+      $resultat = $this->getBdd()->query($sql);    // exécution directe
     }
-}
+    else {
+      $resultat = $this->getBdd()->prepare($sql);  // requête préparée
+      $resultat->execute($params);
+    }
+    return $resultat;
+  }
 
-function updatePost(){
+  // Renvoie un objet de connexion à la BD en initialisant la connexion au besoin
+  private function getBdd() {
+    if ($this->bdd == null) {
+      // Création de la connexion
+      $this->bdd = new PDO('mysql:host=mysql-projet4.alwaysdata.net;dbname=projet4_blog;charset=utf8', 'projet4', 'Moonlight120', 
+                            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }
+    return $this->bdd;
+  }
 
-    $bdd = getBdd();
-    $update = $bdd->prepare('UPDATE T_BILLET SET BIL_TITRE = ?, BIL_CONTENU = ?, 
-                             BIL_DATE_EDITION = NOW() WHERE BIL_ID = ?');
-    $update->execute(array($post_title, $post_content, $edit_id));
-    return $update;
 }
 
 
