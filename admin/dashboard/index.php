c17a5d71192e8d76dd282ca1607539feac9b7e27
Laugel Yannick <laugel-yannick@hotmail.fr>
2019-04-15 13:48:54 +0200

    Passage de l'index à un controller

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -1,17 +1,34 @@
 <?php 
 
-require 'model.php';
+require ('controler/controler.php');
+
+try {
+    if (isset($_GET['action'])){
+        if ($_GET['action'] == 'billet'){
+            if (isset($_GET['id'])){
+                $idBillet = intval ($_GET['id']);
+                if ($idBillet != 0)
+                    getPost($idPost);
+                else
+                    throw new Exception("Identifiant de billet invalide");
+            }
+            else    
+                throw new Exception("Identifiant de billet non défini");
+        }
+        else    
+            throw new Exception("Action non valide");
+    }
+    else    
+        index();
+}
+
+catch (Exception $e){
+    error($e->getMessage());
+}
 
 try{
-    $billets = getBillets();
-    require 'viewIndex.php';
+
 }
- catch (Exception $e){
-     $msgErreur = $e->getMessage();
-     require 'viewError.php';
- }
-
-
 
 
 
