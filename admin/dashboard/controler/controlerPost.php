a93a6ae2786976e61b5e843104a1c8e81172698b
Laugel Yannick <laugel-yannick@hotmail.fr>
2019-04-22 16:35:58 +0200

    Ajout d'un Controller pour les posts

---
+++ admin/dashboard/controler/controlerPost.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'model/post.php';
+require_once 'view/view.php';
+
+class ControlerPost{
+
+    private $post;
+
+    public function __construct(){
+        
+        $this->post = new Post();
+    }
+
+    public function post($idPost){
+
+        $post = $this->post->getPost($idPost);
+        $view = new View('ReadPost');
+        $view->generated(array('post' => $post));
+    }
+}
