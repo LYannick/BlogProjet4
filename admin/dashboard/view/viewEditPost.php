d21186e1a697a7bfd25ceb21b70820fcc0cb679c
Laugel Yannick <laugel-yannick@hotmail.fr>
2019-04-25 13:13:46 +0200

    Correction du problème de non-affichage des valeurs + l'édition d'un post

--- admin/dashboard/view/viewEditPost.php
+++ admin/dashboard/view/viewEditPost.php
@@ -1,19 +1,16 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <?php $this->title = 'Edition'; ?>
 
+
+<head>
     <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=agywk2mah7awl6s8vypttkkgp2xc4ucqg246fwcg207z8emv"></script>
     <script>tinymce.init({ selector: '#mytextarea' });</script>
 </head>
 
- 
+<form method="post" action="index.php?action=editpost">
+    <input type="hidden" name="id" id="id" value="<?= $_GET['id'];?>"/>
+    <input type="text" name="titre" required  value="<?= $edit['BIL_TITRE'] ?>"/>
+    <textarea id="mytextarea" name="article" required><?= $edit['BIL_CONTENU'] ?></textarea>
+    <input type="submit" name="valider" value="Modifier l'article"/>
+</form>
 
-        <form method="post" action="">
-            <input type="text" name="titre" required  value="<?= $edit_post['BIL_TITRE'] ?>"/>
-            <textarea id="mytextarea" name="article" required><?= $edit_post['BIL_CONTENU'] ?></textarea>
-            <input type="submit" name="valider" value="Modifier l'article"/>
-        </form>
 
-</html>
 
