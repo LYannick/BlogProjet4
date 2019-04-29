7d3c4f7af5a0912240bfc09b8cc19310fe8492cd
Laugel Yannick <laugel-yannick@hotmail.fr>
2019-04-29 15:04:33 +0200

    Ajout de l'affichage des commentaires signalés

--- admin/dashboard/view/viewIndex.php
+++ admin/dashboard/view/viewIndex.php
@@ -6,10 +6,10 @@
     <script>tinymce.init({ selector: '#mytextarea' });</script>
   </head>
 
-  <div id="global">
+  <div id="global" class="container">
       <header>
         <h1 id="titreBlog">Administration</h1>
-        <button id="btnAdd" type="button" class="btn btn-primary">Ajouter un article</button>
+        <div id="btnAddPost"><button id="btnAdd" type="button" class="btn btn-primary">Ajouter un article</button><button id="btnHide" type="button" class="btn btn-primary">Masquer l'éditeur</button></div>
       </header>
       <div id ="formAdd">
         <form method="post" action="index.php?action=addpost" enctype="multipart/form-data">
@@ -19,7 +19,7 @@
           <input type="file" name="miniature"/>
       </form>
       </div>
-      <div id="contenu">
+      
       <table class="table table-striped table-dark">
         <thead>
           <tr>
@@ -37,17 +37,47 @@
               <tr>
                 <th scope="row"><?= $post['id'] ?></th>
                 <td><a href="<?= "index.php?action=post&id=" . $post['id'] ?>"><?= $post['titre'] ?></td>
-                <td><?= $post['contenu'] ?></td>
+                <td><?= substr($post['contenu'],0,20) ?></td>
                 <td><?= $post['date'] ?></td>
-                <td><a href="<?= "index.php?action=edit&id=" . $post['id'] ?>"><button type="button" class="btn btn-outline-primary">Modifier</button></a>
-                    <a href="index.php?action=delete&id=<?= $post['id'] ?>"><button type="button" class="btn btn-outline-primary">Supprimer</button></a></td>
+                <td><a href="<?= "index.php?action=edit&id=" . $post['id'] ?>"><button type="button" class="btn btn-primary">Modifier</button></a>
+                    <a href="index.php?action=delete&id=<?= $post['id'] ?>"><button type="button" class="btn btn-primary">Supprimer</button></a></td>
             </tr>
             
-        <?php } ?>
-      
+        <?php } 
+        
+        $posts->closeCursor(); 
+        ?>
       </tbody>
       </table>
-      </div>
+      
+      
+
+      <table class="table table-hover table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Auteur</th>
+            <th scope="col">Contenu</th>
+            <th scope="col">Date</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+
+      <?php foreach ($reports as $report): ?>
+          
+      <tr>
+          <th scope="row"><?= $report['id'] ?></th>
+          <td><?= $report['author'] ?></td>
+          <td><?= substr($report['content'],0,20) ?></td>
+          <td><?= $report['date'] ?></td>
+          <td><a href="index.php?action=delete&id=<?= $report['id'] ?>"><button type="button" class="btn btn-primary">Supprimer</button></a>
+              <a href="index.php?action=delete&id=<?= $report['id'] ?>"><button type="button" class="btn btn-primary">Approuver</button></a></td>
+      </tr>
+      <?php endforeach; ?>
+
+      </tbody>
+      </table>
       </div>
 
       <footer>
