ab11ddab211e3f789e8ac21365c61db95fbac63a
Laugel Yannick <laugel-yannick@hotmail.fr>
2019-04-29 15:05:53 +0200

    Ajout de l'affichage des commentaires signalés

--- admin/dashboard/controler/controlerIndex.php
+++ admin/dashboard/controler/controlerIndex.php
@@ -7,7 +7,7 @@ require_once 'view/view.php';
 class ControlerIndex{
 
     private $post;
-    private $add;
+    private $admin;
 
     public function __construct(){
 
@@ -17,9 +17,13 @@ class ControlerIndex{
 
     public function index(){
         
+        $reports = $this->admin->getReportCom();
         $posts = $this->post->getPosts();
         $view = new View("Index");
-        $view->generated(array('posts' => $posts));
+        $view->generated(array(
+            'posts' => $posts,
+            'reports' => $reports
+        ));
     }
 
     public function add($title, $content){
@@ -33,4 +37,5 @@ class ControlerIndex{
         $this->admin->deletePost($delete_id);
         $this->index();
     }
+
 }
