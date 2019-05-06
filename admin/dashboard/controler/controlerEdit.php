58ab790f82af3dface146111a5f61dae308d2c58
Laugel Yannick <laugel-yannick@hotmail.fr>
2019-05-06 12:29:26 +0200

    Modification sur le controler

--- admin/dashboard/controler/controlerEdit.php
+++ admin/dashboard/controler/controlerEdit.php
@@ -6,31 +6,21 @@ require_once 'view/view.php';
 class ControlerEdit{
 
     private $edit;
-    private $editPost;
 
     public function __construct(){
         
         $this->edit = new Admin();
-        $this->editPost = new Admin();
     }
 
     public function edit($edit_id){
 
-        $post = $this->edit->editPosts($edit_id);
+        $edit = $this->edit->editPosts($edit_id);
         $view = new View('EditPost');
         $view->generated(array('edit' => $edit));
     }
 
     public function editPost($edit_title, $edit_content, $edit_id){
 
-        $this->editPost->updatePost($edit_title, $edit_content, $edit_id);
-        $this->index();
+        $this->edit->updatePost($edit_title, $edit_content, $edit_id);
     }
-
-    public function index(){
-        
-        $posts = $this->post->getPosts();
-        $view = new View("Index");
-        $view->generated(array('posts' => $posts));
-    }
-}
+}   
